a68579494fb54f04c14b69323bf40123a8eab3a9
cx-rotems <rotem.sheps@checkmarx.com>
2025-01-16 23:32:01 +0200

    result size

--- processors/minio_extractor.go
+++ processors/minio_extractor.go
@@ -21,7 +21,7 @@ func (me *MinioExtractor) Start() {
 
 	for job := range me.minioChan {
 	//	fmt.Printf("MinioExtractor: Extracting data for job ID %d\n", job.ID)
-		for i := 1; i < 50; i++ {
+		for i := 1; i <= 50; i++ {
 			time.Sleep(100 * time.Millisecond) // simulate download from Minio
 			me.resultChan <- types.Result{ResultID: i, JobID: job.ID}
 		}
